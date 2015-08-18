1ce59626ee5bec791bb4ccd1876dfc89c7cd746e
Kejun Xia <ideablast@gmail.com>
2015-08-18 20:25:13 +1000

    Add test for view pager life cycles

---
+++ library/android-mvc-test/src/main/java/com/shipdream/lib/android/mvc/view/viewpager/BaseTabFragment.java
@@ -0,0 +1,67 @@
+package com.shipdream.lib.android.mvc.view.viewpager;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.shipdream.lib.android.mvc.view.MvcFragment;
+import com.shipdream.lib.android.mvc.view.help.LifeCycleMonitor;
+
+public abstract class BaseTabFragment extends MvcFragment {
+    protected abstract LifeCycleMonitor getLifeCycleMonitor();
+    
+    @Override
+    public void onViewReady(View view, Bundle savedInstanceState, MvcFragment.Reason reason) {
+        getLifeCycleMonitor().onCreateView(view, savedInstanceState);
+        getLifeCycleMonitor().onViewCreated(view, savedInstanceState);
+        super.onViewReady(view, savedInstanceState, reason);
+        getLifeCycleMonitor().onViewReady(view, savedInstanceState, reason);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getLifeCycleMonitor().onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getLifeCycleMonitor().onResume();
+    }
+
+    @Override
+    protected void onReturnForeground() {
+        super.onReturnForeground();
+        getLifeCycleMonitor().onReturnForeground();
+    }
+
+    @Override
+    protected void onPushingToBackStack() {
+        super.onPushingToBackStack();
+        getLifeCycleMonitor().onPushingToBackStack();
+    }
+
+    @Override
+    protected void onPoppedOutToFront() {
+        super.onPoppedOutToFront();
+        getLifeCycleMonitor().onPoppedOutToFront();
+    }
+
+    @Override
+    protected void onOrientationChanged(int lastOrientation, int currentOrientation) {
+        super.onOrientationChanged(lastOrientation, currentOrientation);
+        getLifeCycleMonitor().onOrientationChanged(lastOrientation, currentOrientation);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        getLifeCycleMonitor().onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+        getLifeCycleMonitor().onDestroy();
+        super.onDestroy();
+    }
+}
--- library/android-mvc-test/src/main/java/com/shipdream/lib/android/mvc/view/viewpager/ViewPagerHomeFragment.java
+++ library/android-mvc-test/src/main/java/com/shipdream/lib/android/mvc/view/viewpager/ViewPagerHomeFragment.java
@@ -7,7 +7,9 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 
+import com.shipdream.lib.android.mvc.view.MvcApp;
 import com.shipdream.lib.android.mvc.view.MvcFragment;
+import com.shipdream.lib.android.mvc.view.help.LifeCycleMonitor;
 import com.shipdream.lib.android.mvc.view.test.R;
 
 public class ViewPagerHomeFragment extends MvcFragment {
@@ -15,6 +17,8 @@ public class ViewPagerHomeFragment extends MvcFragment {
 
     private PagerAdapter pagerAdapter;
 
+    private LifeCycleMonitor lifeCycleMonitor = MvcApp.lifeCycleMonitorFactory.provideLifeCycleMonitor();
+    
     @Override
     protected int getLayoutResId() {
         return R.layout.fragment_view_pager_home;
@@ -22,13 +26,66 @@ public class ViewPagerHomeFragment extends MvcFragment {
 
     @Override
     public void onViewReady(View view, Bundle savedInstanceState, Reason reason) {
+        lifeCycleMonitor.onCreateView(view, savedInstanceState);
+        lifeCycleMonitor.onViewCreated(view, savedInstanceState);
         super.onViewReady(view, savedInstanceState, reason);
+        lifeCycleMonitor.onViewReady(view, savedInstanceState, reason);
+
         viewPager = (ViewPager) view.findViewById(R.id.viewpager);
         if (reason == Reason.FIRST_TIME || reason == Reason.RESTORE) {
             pagerAdapter = new PagerAdapter(getChildFragmentManager());
         }
 
         viewPager.setAdapter(pagerAdapter);
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        lifeCycleMonitor.onCreate(savedInstanceState);
+    }
+    
+    @Override
+    public void onResume() {
+        super.onResume();
+        lifeCycleMonitor.onResume();
+    }
+
+    @Override
+    protected void onReturnForeground() {
+        super.onReturnForeground();
+        lifeCycleMonitor.onReturnForeground();
+    }
+
+    @Override
+    protected void onPushingToBackStack() {
+        super.onPushingToBackStack();
+        lifeCycleMonitor.onPushingToBackStack();
+    }
+
+    @Override
+    protected void onPoppedOutToFront() {
+        super.onPoppedOutToFront();
+        lifeCycleMonitor.onPoppedOutToFront();
+    }
+
+    @Override
+    protected void onOrientationChanged(int lastOrientation, int currentOrientation) {
+        super.onOrientationChanged(lastOrientation, currentOrientation);
+        lifeCycleMonitor.onOrientationChanged(lastOrientation, currentOrientation);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        lifeCycleMonitor.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+        lifeCycleMonitor.onDestroy();
+        super.onDestroy();
     }
 
     private class PagerAdapter extends FragmentStatePagerAdapter {
