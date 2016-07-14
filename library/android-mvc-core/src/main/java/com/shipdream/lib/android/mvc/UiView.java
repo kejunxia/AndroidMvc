13d632efb347f5befe196bb2263f2935c112335b
Kejun Xia <ideablast@gmail.com>
2016-07-14 17:04:00 +1000

    Update document

--- library/android-mvc-core/src/main/java/com/shipdream/lib/android/mvc/UiView.java
+++ library/android-mvc-core/src/main/java/com/shipdream/lib/android/mvc/UiView.java
@@ -1,15 +1,46 @@
 package com.shipdream.lib.android.mvc;
 
 /**
- * UiView represents android views. Call it Ui-View not to conflict with android.view.View and
- * simplify imports
+ * <p>
+ * UiView represents android views. It has a single method {@link #update()}. Every {@link Controller}
+ * holds an instance of {@link UiView} which is implemented by the controller's corresponding
+ * concrete view. Whenever the {@link Controller} updates its state, usually it's model, the controller
+ * needs to call {@link UiView}.{@link #update()}.
+ * </p>
+ *
+ * <p>
+ * Usually this is enough for most Android views since as long as the concrete view implements
+ * {@link UiView}.{@link #update()} by binding the full controller model to the view, it guarantees the view's
+ * graphics is always in sync with the controller's model. However, if the model of a controller is
+ * large and sometimes only very limited part of the model changes, you may not always want to call
+ * {@link UiView}.{@link #update()} to rebind the entire model to the view if performance becomes a concern.
+ * </p>
+ *
+ * <p>
+ * In this case, define a custom view interface extending {@link UiView} with extra method to allow
+ * controllers updating specific part of the view. For example,
+ * </p>
+ *
+ * <pre>
+ * public interface PageView extends UiView {
+ *      /**
+ *      * Let the controller of the page view just update the title instead calling {@link UiView}.{@link #update()}
+ *      * to update the whole page.
+ *      * @param title The title of the page.
+ *      * /
+ *      void updateTitle(String title);
+ * }
+ * </pre>
  */
 public interface UiView {
     /**
      * When a view is requested to update itself, it should read it's controller's model by
      * {@link Controller#getModel()} to bind the data to the view.
      *
-     * <p>Do NOT change values of model from view but only from controllers.</p>
+     * <p>It will be automatically called when a view is ready to shown. Controllers should call this
+     * method when they updated their model.</p>
+     *
+     * <p><b>Do NOT change values of model from view but only from controllers.</b></p>
      */
     void update();
 }
